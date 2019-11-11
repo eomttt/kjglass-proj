8f89d23c61174ae029e8aa69ba2ead9f26ff102a
eomttt <hyunt0413@gmail.com>
2019-11-12 00:18:04 +0900

    feat: Add processing glass layout

---
+++ components/equipment/glassProcessing.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const GlassProcessing = ({ imageUrl }) => {
+    return (
+        <>
+        </>
+    );
+};
+
+GlassProcessing.propTypes = {
+    imageUrl: PropTypes.string.isRequired,
+};
+
+export default GlassProcessing;
