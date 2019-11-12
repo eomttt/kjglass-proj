9789ebcd36b91b57e4bafdb6954df768ca274dd1
eomttt <hyunt0413@gmail.com>
2019-11-12 10:09:21 +0900

    feat: Add dummy

---
+++ containers/equipment/glassProcessings.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+import glassProcessingsInfo from '../../dummy/glassProcessing';
+
+const GlassProcessings = () => {
+    const [glassProcessings] = useState(glassProcessingsInfo);
+
+    return (
+        
+    )
+};
+
+export default GlassProcessings;
---
+++ dummy/glassProcessing.js
@@ -0,0 +1,20 @@
+const glassProcessing = {
+    one_neck_flask: {
+        title: '1-Neck-Flask 가공',
+        image: 'https://firebasestorage.googleapis.com/v0/b/kjglass-60495.appspot.com/o/equipments%2Fproccess_1neck%2F1_neck_flask_1.png?alt=media&token=b83cd09c-772a-4c2b-94fc-b48a0196d2fd',
+    },
+    three_neck_flask: {
+        title: '3-Neck-Flask 가공',
+        image: 'https://firebasestorage.googleapis.com/v0/b/kjglass-60495.appspot.com/o/equipments%2Fproccess_1neck%2Fneck_flask_3.png?alt=media&token=e6feabb2-bc0d-40ae-90ac-01645bb70beb',
+    },
+    condenser: {
+        title: 'Condenser 가공',
+        image: 'https://firebasestorage.googleapis.com/v0/b/kjglass-60495.appspot.com/o/equipments%2Fproccess_1neck%2Fcondenser.png?alt=media&token=e6950686-4150-419f-9f7d-d5eb4852691f',
+    },
+    etc: {
+        title: '기타',
+        image: 'https://firebasestorage.googleapis.com/v0/b/kjglass-60495.appspot.com/o/equipments%2Fproccess_1neck%2Fetc.png?alt=media&token=306ea0ca-1d5e-47b9-9484-ec21bd478fa8',
+    },
+};
+
+export default glassProcessing;
