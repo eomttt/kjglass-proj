2bed492c098fe128b6aacfa9bf894f27e4cfaacd
eomttt <hyunt0413@gmail.com>
2021-12-14 11:42:20 +0900

    Vercel mail test

--- server.mailer.js
+++ server.mailer.js
@@ -2,44 +2,40 @@ const nodemailer = require('nodemailer');
 
 const config = require('./mail.config');
 
-const SEND_TO_MAIL = '733@hanmail.net';
+const SEND_TO_MAIL = 'hyunt0413@gmail.com';
 
 const transporter = nodemailer.createTransport({
-    service: config.mailer.service,
-    auth: {
-        user: config.mailer.user,
-        pass: config.mailer.pass,
-    },
+  service: config.mailer.service,
+  auth: {
+    user: config.mailer.user,
+    pass: config.mailer.pass,
+  },
 });
 
-const translateMail = (data) => new Promise((resolve, reject) => {
+const translateMail = (data) =>
+  new Promise((resolve, reject) => {
     const { contactData, item } = data;
-    const {
-        title, company, team, name, email, number, faxNum, content,
-    } = contactData;
+    const { title, company, team, name, email, number, faxNum, content } =
+      contactData;
     const { glass, expendables } = item;
 
     const glassInfoItems = glass.map((glassData) => {
-        const { url, count, selectedSpecificItem } = glassData;
-        const { number: catalogNumber } = selectedSpecificItem;
-        return (
-            `<div><div>URL: ${url}</div><div>카타로그 번호: ${catalogNumber}</div><div>주문 갯수: ${count}</div></div><br>`
-        );
+      const { url, count, selectedSpecificItem } = glassData;
+      const { number: catalogNumber } = selectedSpecificItem;
+      return `<div><div>URL: ${url}</div><div>카타로그 번호: ${catalogNumber}</div><div>주문 갯수: ${count}</div></div><br>`;
     });
 
     const expendableItems = expendables.map((expendableData) => {
-        const { url, count, selectedSpecificItem } = expendableData;
-        const { number: catalogNumber } = selectedSpecificItem;
-        return (
-            `<div><div>URL: ${url}</div><div>카타로그 번호: ${catalogNumber}</div><div>주문 갯수: ${count}</div></div><br>`
-        );
+      const { url, count, selectedSpecificItem } = expendableData;
+      const { number: catalogNumber } = selectedSpecificItem;
+      return `<div><div>URL: ${url}</div><div>카타로그 번호: ${catalogNumber}</div><div>주문 갯수: ${count}</div></div><br>`;
     });
 
     const mailOptions = {
-        from: 'FROM < hyunt0413@naver.com >',
-        to: SEND_TO_MAIL,
-        subject: '견적 문의',
-        html: `<div><b>제목: ${title}</b></div>
+      from: 'FROM < hyunt0413@naver.com >',
+      to: SEND_TO_MAIL,
+      subject: '견적 문의',
+      html: `<div><b>제목: ${title}</b></div>
                <br>
                <div>회사: ${company}</div>
                <br>
@@ -62,20 +58,22 @@ const translateMail = (data) => new Promise((resolve, reject) => {
                <div>${glassInfoItems.map((glassItem) => glassItem)}</div>
                <br>
                <div><b>기타 화학 제품군</b></div>
-               <div>${expendableItems.map((expendableItem) => expendableItem)}</div>`,
+               <div>${expendableItems.map(
+                 (expendableItem) => expendableItem
+               )}</div>`,
     };
 
     transporter.sendMail(mailOptions, (err, res) => {
-        if (err) {
-            console.log('failed... => ', err);
-            reject(err);
-        } else {
-            console.log('succeed... => ', res);
-            resolve(res);
-        }
+      if (err) {
+        console.log('failed... => ', err);
+        reject(err);
+      } else {
+        console.log('succeed... => ', res);
+        resolve(res);
+      }
 
-        transporter.close();
+      transporter.close();
     });
-});
+  });
 
 module.exports.translateMail = translateMail;
