2e04f54c3d76d96746d78e81e7881d6a821fc5b7
eomttt <hyunt0413@gmail.com>
2021-12-15 12:37:35 +0900

    Mail sender api 설정

--- containers/customercenter/Contact.jsx
+++ containers/customercenter/Contact.jsx
@@ -1,68 +1,68 @@
-import React, { useEffect } from 'react';
+import axios from 'axios';
 import { toJS } from 'mobx';
 import { observer } from 'mobx-react';
-
-import axios from 'axios';
-
+import React, { useEffect } from 'react';
+import ContactComp from '../../components/customercenter/Contact';
 import useStore from '../../hooks/useStore';
 
-import ContactComp from '../../components/customercenter/Contact';
-
 const Contact = () => {
-    const { bascketStore, itemsStore } = useStore();
-    const { glasses, expendables } = toJS(itemsStore);
-    const { glassItems } = toJS(bascketStore);
+  const { bascketStore, itemsStore } = useStore();
+  const { glasses, expendables } = toJS(itemsStore);
+  const { glassItems } = toJS(bascketStore);
 
-    useEffect(() => {
-        // Nothing. But to update component
-    }, [glasses, expendables]);
+  useEffect(() => {
+    // Nothing. But to update component
+  }, [glasses, expendables]);
 
-    const convertItem = (items, type) => items.map((item) => {
-        const itemInfo = itemsStore.getItemInfo({
-            type,
-            itemId: item.itemId,
-            specificationId: item.specificationItemId,
-        });
+  const convertItem = (items, type) =>
+    items.map((item) => {
+      const itemInfo = itemsStore.getItemInfo({
+        type,
+        itemId: item.itemId,
+        specificationId: item.specificationItemId,
+      });
 
-        return {
-            ...itemInfo,
-            count: item.count,
-            url: `http://kjglass.co.kr/shop?id=${type === 'glasses' ? 1 : 2}&classifiedId=${encodeURI(itemInfo.selectedItem.classify)}&productId=${item.itemId}`,
-        };
+      return {
+        ...itemInfo,
+        count: item.count,
+        url: `http://kjglass.co.kr/shop?id=${
+          type === 'glasses' ? 1 : 2
+        }&classifiedId=${encodeURI(itemInfo.selectedItem.classify)}&productId=${
+          item.itemId
+        }`,
+      };
     });
 
-    const submitContact = async (contactData) => {
-        const { email, number } = contactData;
+  const submitContact = async (contactData) => {
+    const { email, number } = contactData;
 
-        if (email || number) {
-            alert('장바구니에 담긴 정보가 함께 전송 됩니다.');
-            try {
-                await axios.post('/translate-mail', {
-                    contactData,
-                    item: {
-                        glass: convertItem(glassItems.glass, 'glasses'),
-                        expendables: convertItem(glassItems.expendables, 'expendables'),
-                    },
-                });
-                alert('성공적으로 전송 되었습니다.');
-            } catch (error) {
-                alert(`에러가 발생 하였습니다${error}`);
-            }
-        } else {
-            alert('이메일 또는 전화번호를 남겨주세요.');
-        }
-    };
+    if (email || number) {
+      alert('장바구니에 담긴 정보가 함께 전송 됩니다.');
+      try {
+        await axios.post(
+          'https://i6lostn24e.execute-api.ap-northeast-2.amazonaws.com/dev/send-email',
+          {
+            contactData,
+            item: {
+              glass: convertItem(glassItems.glass, 'glasses'),
+              expendables: convertItem(glassItems.expendables, 'expendables'),
+            },
+          }
+        );
+        alert('성공적으로 전송 되었습니다.');
+      } catch (error) {
+        alert(`에러가 발생 하였습니다${error}`);
+      }
+    } else {
+      alert('이메일 또는 전화번호를 남겨주세요.');
+    }
+  };
 
-    const openKakao = () => {
-        window.open('https://open.kakao.com/o/szj7uKQb');
-    };
+  const openKakao = () => {
+    window.open('https://open.kakao.com/o/szj7uKQb');
+  };
 
-    return (
-        <ContactComp
-            submitContact={submitContact}
-            openKakao={openKakao}
-        />
-    );
+  return <ContactComp submitContact={submitContact} openKakao={openKakao} />;
 };
 
 export default observer(Contact);
